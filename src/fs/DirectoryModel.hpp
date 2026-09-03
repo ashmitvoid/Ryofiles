2f9a69c3a83e97ad4f3cc05cbbd61a74b468cb9b
ASHMIT <52139594+ashmitvoid@users.noreply.github.com>
2026-09-04 03:08:08 +0530

    feat: add in-memory current-folder filtering

--- src/fs/DirectoryModel.hpp
+++ src/fs/DirectoryModel.hpp
@@ -5,6 +5,7 @@
 #include <QFileInfo>
 #include <QFileSystemWatcher>
 #include <QTimer>
+#include <QVector>
 
 class DirectoryModel : public QAbstractListModel {
     Q_OBJECT
@@ -12,7 +13,9 @@ class DirectoryModel : public QAbstractListModel {
     Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged)
     Q_PROPERTY(bool loading READ loading NOTIFY loadingChanged)
     Q_PROPERTY(bool showHidden READ showHidden WRITE setShowHidden NOTIFY showHiddenChanged)
+    Q_PROPERTY(QString filterText READ filterText WRITE setFilterText NOTIFY filterTextChanged)
     Q_PROPERTY(int count READ rowCount NOTIFY countChanged)
+    Q_PROPERTY(int unfilteredCount READ unfilteredCount NOTIFY countChanged)
 
     Q_PROPERTY(QString home READ home CONSTANT)
     Q_PROPERTY(QString desktop READ desktop CONSTANT)
@@ -47,6 +50,10 @@ public:
     bool showHidden() const { return m_showHidden; }
     void setShowHidden(bool show);
 
+    QString filterText() const { return m_filterText; }
+    void setFilterText(const QString& text);
+    int unfilteredCount() const { return m_allEntries.size(); }
+
     QString home() const;
     QString desktop() const;
     QString documents() const;
@@ -58,6 +65,7 @@ public:
     Q_INVOKABLE void refresh();
     Q_INVOKABLE void goUp();
     Q_INVOKABLE void activate(int index);
+    Q_INVOKABLE void clearFilter();
 
     Q_INVOKABLE QString pathAt(int index) const;
     Q_INVOKABLE bool isDirectoryAt(int index) const;
@@ -67,6 +75,7 @@ signals:
     void pathChanged();
     void loadingChanged();
     void showHiddenChanged();
+    void filterTextChanged();
     void countChanged();
     void errorOccurred(const QString& message);
 
@@ -84,13 +93,18 @@ private:
     void scan();
     void watchCurrentDirectory();
     void setLoading(bool loading);
+    void rebuildFilterIndexes();
+    const Entry* entryForRow(int row) const;
+
     static QList<Entry> scanDirectory(const QString& path, bool showHidden, QString* error);
     static QString formatSize(qint64 bytes);
     static QString standardPath(int location);
     static bool thumbnailCandidateFor(const QFileInfo& info);
 
-    QList<Entry> m_entries;
+    QList<Entry> m_allEntries;
+    QVector<int> m_visibleIndexes;
     QString m_path;
+    QString m_filterText;
     bool m_loading = false;
     bool m_showHidden = false;
     quint64 m_generation = 0;
